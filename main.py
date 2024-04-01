6b1f91cc6b280af8a4cf182fd7d40a91de2e7804
krylovolegv <krylovolegv@yandex.ru>
2024-04-01 14:04:40 +0200

    add live mouse every 1 sec

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import pygame
 import random
 
-from pygame import event
-
 pygame.init()
 
 SCREEN_WIDTH = 800
@@ -14,14 +12,15 @@ icon = pygame.image.load("img/1612407962_17-p-tir-risunok-18.jpg")
 pygame.display.set_icon(icon)
 
 target_img = pygame.image.load("img/target_01.png")
-target_width = 50
-target_height = 50
+target_width, target_height = 50, 50
+target_x = random.randint(0, SCREEN_WIDTH - target_width)
+target_y = random.randint(0, SCREEN_HEIGTH - target_height)
 
-target_x = random.randint (0, SCREEN_WIDTH - target_width)
-target_y = random.randint (0, SCREEN_HEIGTH - target_height)
+color = random.randint(0, 155), random.randint(0, 155), random.randint(0, 155)
 
-color = random.randint (0, 155), random.randint (0, 155), random.randint (0, 155)
-
+# Создаем пользовательское событие для перемещения мишени
+MOVE_TARGET_EVENT = pygame.USEREVENT + 1
+pygame.time.set_timer(MOVE_TARGET_EVENT, 1000)  # Таймер для перемещения мишени каждые 5 секунд
 
 running = True
 while running:
@@ -29,14 +28,17 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        if event.type == pygame.MOUSEBUTTONDOWN:
+        elif event.type == MOVE_TARGET_EVENT:
+            # Перемещаем мишень
+            target_x = random.randint(0, SCREEN_WIDTH - target_width)
+            target_y = random.randint(0, SCREEN_HEIGTH - target_height)
+        elif event.type == pygame.MOUSEBUTTONDOWN:
             mouse_x, mouse_y = pygame.mouse.get_pos()
             if target_x < mouse_x < target_x + target_width and target_y < mouse_y < target_y + target_height:
                 target_x = random.randint(0, SCREEN_WIDTH - target_width)
                 target_y = random.randint(0, SCREEN_HEIGTH - target_height)
+
     screen.blit(target_img, (target_x, target_y))
-
     pygame.display.update()
-
 
 pygame.quit()
